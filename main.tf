9f469be7aaccd61b348837aa716ff86f4981268e
Alex Mendes <alexanderhmendes@gmail.com>
2019-10-12 21:23:58 +0100

    chore: use latest version of tf-aws-lambda-api

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "lambda_api" {
-  source      = "git::https://github.com/alexandermendes/tf-aws-lambda-api.git?ref=master"
+  source      = "git::https://github.com/alexandermendes/tf-aws-lambda-api.git?ref=tags/v1.3.0"
   name        = "clone_ghe_repo"
   dir         = "${path.module}/functions"
   ext         = "py"
