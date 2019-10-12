af3aaa59c708f16c07a256c44aa98814fca40ef2
Alex Mendes <alexanderhmendes@gmail.com>
2019-10-12 23:58:11 +0100

    feat: add webhook secret

--- main.tf
+++ main.tf
@@ -1,9 +1,17 @@
+resource "random_password" "webhook_secret" {
+  length = 32
+  special = true
+}
+
 module "lambda_api" {
-  source      = "git::https://github.com/alexandermendes/tf-aws-lambda-api.git?ref=dev"
+  source      = "git::https://github.com/alexandermendes/tf-aws-lambda-api.git?ref=tags/v1.3.1"
   name        = "clone_ghe_repo"
   dir         = "${path.module}/functions"
   ext         = "py"
   runtime     = "python3.7"
   handler     = "lambda_handler"
   http_method = "POST"
+  environment = {
+    GITHUB_WEBHOOK_SECRET = random_password.webhook_secret.result
+  }
 }
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "webhook_url" {
   value       = module.lambda_api.invoke_url
   description = "The webhook URL to invoke the Lambda function"
 }
+
+output "webhook_secret" {
+  value       = random_password.webhook_secret.result
+  description = "The webhook secret."
+}
