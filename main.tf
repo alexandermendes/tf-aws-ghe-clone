8431c3aceb5e529324699f05861c952a7a7fb2f8
Alex Mendes <alexanderhmendes@gmail.com>
2019-10-13 01:37:50 +0100

    feat: add git lambda layer

--- main.tf
+++ main.tf
@@ -3,13 +3,19 @@ resource "random_password" "webhook_secret" {
 }
 
 module "lambda_api" {
-  source      = "git::https://github.com/alexandermendes/tf-aws-lambda-api.git?ref=tags/v1.3.1"
+  source      = "git::https://github.com/alexandermendes/tf-aws-lambda-api.git?ref=tags/v1.4.0"
   name        = "clone-ghe-repo"
   dir         = "${path.module}/functions"
   ext         = "js"
   runtime     = "nodejs8.10"
   handler     = "handler"
   http_method = "POST"
+
+  # https://github.com/lambci/git-lambda-layer
+  layers      = [
+    "arn:aws:lambda:${var.aws_region}:553035198032:layer:git:6"
+  ]
+
   environment = {
     variables = {
       GITHUB_WEBHOOK_SECRET = random_password.webhook_secret.result
---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "aws_region" {
+  description = "The AWS region within which the resource are being created."
+}
