1b9726bfab88a501ad6897fdfe8bfee276ec0122
Alex Mendes <alexanderhmendes@gmail.com>
2019-10-14 22:25:48 +0100

    fix: iam policy assume role id

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ data "aws_iam_policy_document" "assume_role_policy" {
 
     principals {
       type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
+      identifiers = ["codepipeline.amazonaws.com"]
     }
   }
 }
