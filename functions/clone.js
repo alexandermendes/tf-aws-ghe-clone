c847cee0bea34a41dbf03176e54ef1d69477f00b
Alex Mendes <alexanderhmendes@gmail.com>
2019-10-16 23:13:07 +0100

    feat: validate github webhook with secret

--- functions/clone.js
+++ functions/clone.js
@@ -41,12 +41,50 @@ const uploadToS3 = async (zipPath) => {
 };
 
 /**
+ * Get the GutHub webhook hash signature.
+ *
+ * https://developer.github.com/webhooks/securing/
+ */
+const getSignature = () => {
+  const hmac = crypto.createHmac('sha1', process.env.GITHUB_WEBHOOK_SECRET);
+  const hex = hmac.update(chunk.toString()).digest('hex');
+
+  return `sha1=${hex}`;
+}
+
+/**
+ * Get the Lambda proxy output.
+ *
+ * https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ */
+const getJsonResponse = ({
+  statusCode = 200,
+  headers = {},
+  body = '',
+  isBase64Encoded = false,
+}) => {
+  return JSON.stringify({
+    isBase64Encoded,
+    statusCode,
+    headers,
+    body,
+  });
+}
+
+/**
  * Run.
  */
-exports.handler = async (event) => {
-  const { repository } = event;
+exports.handler = async (event, context, callback) => {
+  const { headers } = event;
+  const { repository } = context;
+
+  if (headers['X-Hub-Signature'] !== getSignature()) {
+    callback(getJsonResponse(403));
+  }
 
   const zipPath = cloneAndZip(repository);
 
   await uploadToS3(zipPath);
+
+  callback(getJsonResponse(200))
 }
