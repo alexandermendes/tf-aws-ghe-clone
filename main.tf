531fbf7387a72b3a41cd4a302649752d6b92f1fe
Alex Mendes <alexanderhmendes@gmail.com>
2019-10-20 11:43:37 +0100

    chore: add wip scheduled lambda resources

--- main.tf
+++ main.tf
@@ -76,3 +76,25 @@ module "lambda" {
     }
   }
 }
+
+resource "aws_cloudwatch_event_rule" "every_five_minutes" {
+    name = "every-five-minutes"
+    description = "Fires every five minutes"
+    schedule_expression = "rate(5 minutes)"
+}
+
+
+resource "aws_cloudwatch_event_target" "check_foo_every_five_minutes" {
+    rule = aws_cloudwatch_event_rule.every_five_minutes.name
+    target_id = "check_foo"
+    arn = module.lambda.arn
+}
+
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_check_foo" {
+    statement_id = "AllowExecutionFromCloudWatch"
+    action = "lambda:InvokeFunction"
+    function_name = module.lambda.function_name
+    principal = "events.amazonaws.com"
+    source_arn = aws_cloudwatch_event_rule.every_five_minutes.arn
+}
